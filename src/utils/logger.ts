2c3f76584c3a835b9450e8ab3c2c4e641ca6b789
yoratoni <71235233+yoratoni@users.noreply.github.com>
2023-06-21 01:18:40 +0200

    Removed relative import

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,7 +1,6 @@
+import { GENERAL_CONFIG } from "configs/global.config";
 import { createLogger, format, transports } from "winston";
 import Transport from "winston-transport";
-
-import { GENERAL_CONFIG } from "../configs/global.config";
 
 
 /**
