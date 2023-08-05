7fb4c9484e605373a2096343e62cfd1b799bb6df
yoratoni <71235233+yoratoni@users.noreply.github.com>
2023-08-05 03:55:05 +0200

    Working on strategies

--- src/strategies/intraday.ts
+++ src/strategies/intraday.ts
@@ -1,4 +1,5 @@
-import { NsStrategy } from "../utils/imports"
+import { NsStrategy } from "../utils/imports";
+import logger from "../utils/logger";
 
 
 /**
@@ -10,8 +11,45 @@ import { NsStrategy } from "../utils/imports"
 export default function run(
     storage: NsStrategy.storage
 ): NsStrategy.storage {
+    const priceBar = storage.priceBars[
+        storage.priceBars.length - 1
+    ];
 
-    console.log("Intraday strategy running...");
+    logger.info(`Price bar PCT change: ${priceBar.pctChange}`);
+
+    if (!storage.inPosition) {
+        if (priceBar.pctChange && priceBar.pctChange > 0.01 && priceBar.forwardLookingBias) {
+            storage.lastBuyPrice = priceBar.forwardLookingBias;
+            storage.targetProfit = storage.lastBuyPrice * 1.02;
+            storage.stopLoss = storage.lastBuyPrice * 0.98;
+
+            storage.inPosition = true;
+
+            logger.info(
+                `Bought at ${storage.lastBuyPrice} (target profit: ${storage.targetProfit}, stop loss: ${storage.stopLoss})`
+            );
+        }
+    }
+
+    if (storage.inPosition) {
+        if (priceBar.high > storage.targetProfit) {
+            const profit = ((storage.targetProfit - storage.lastBuyPrice) / storage.lastBuyPrice) - 0.001;
+            storage.rawProfits.push(profit);
+
+            storage.inPosition = false;
+
+            logger.info(`Sold at ${storage.targetProfit} (target profit)`);
+        }
+
+        if (priceBar.low < storage.stopLoss) {
+            const profit = ((storage.stopLoss - storage.lastBuyPrice) / storage.lastBuyPrice) - 0.001;
+            storage.rawProfits.push(profit);
+
+            storage.inPosition = false;
+
+            logger.info(`Sold at ${storage.stopLoss} (stop loss)`);
+        }
+    }
 
     return storage;
 }
--- src/utils/exports.ts
+++ src/utils/exports.ts
@@ -1,12 +1,10 @@
 /**
  * Links strategy exports to the Genesis repo.
- * As all these function have a "run" function,
- * we rename them to "strategyName__run".
  */
 
-import intraday__run from "../strategies/intraday";
+import intraday from "../strategies/intraday";
 
 
 export {
-    intraday__run
+    intraday
 }
